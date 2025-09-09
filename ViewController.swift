c141f68bcf6435f454fc012a1cfc6735dd517843
Rafael Teles <rrft@cin.ufpe.br>
2025-09-09 18:06:24 -0300

    init

---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by rafitels
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
+    
+    let diceArray = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")
+    ]
+   
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+}
+
+
+
